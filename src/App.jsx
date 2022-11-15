587a7c27c170055a134cda6b4ab214934f146cc2
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-11-15 16:15:42 +0100

    Add Favicon + Prettier stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 function App() {
-	return (
-		<div className="App container mx-auto bg-blazePrimary200">
-			<Navbar />
-		</div>
-	);
+    return (
+        <div className="App container mx-auto bg-blazePrimary200">
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,21 @@ import Button from "../UI/Button";
 import SearchBar from "../UI/SearchBar";
 import NavLinks from "./NavLinks";
 export default function Navbar() {
-	return (
-		<div className="flex justify-between items-center text-white h-14 rounded-b-lg px-16 bg-blazePimary">
-			{/* Logo */}
-			<div className="w-9">
-				<a href="">
-					<img src={Logo} alt="" />
-				</a>
-			</div>
-			{/* Links */}
-			<NavLinks />
-			<div className="flex justify-center items-center space-x-2">
-				<SearchBar />
-				<Button label="Register" />
-				<Button label="Login" outline={true} />
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex justify-between items-center text-white h-14 rounded-b-lg px-16 bg-blazePimary">
+            {/* Logo */}
+            <div className="w-9">
+                <a href="">
+                    <img src={Logo} alt="" />
+                </a>
+            </div>
+            {/* Links */}
+            <NavLinks />
+            <div className="flex justify-center items-center space-x-2">
+                <SearchBar />
+                <Button label="Register" />
+                <Button label="Login" outline={true} />
+            </div>
+        </div>
+    );
 }
